541bd7b242ff9a65a2ce90e4bf97eb4d3592a2bf
quki <quki09@gmail.com>
2016-06-07 23:21:42 +0900

    update posture part (count method)

--- Arduino/AlphaChairArduino/AlphaChairArduino.ino
+++ Arduino/AlphaChairArduino/AlphaChairArduino.ino
@@ -1,5 +1,11 @@
 #include <SoftwareSerial.h>
 #include <Thread.h>
+#include <QueueList.h>
+
+QueueList <int> q;
+int sum ;
+int firstVal;
+
 #define PPR_ON "a"
 #define PPR_OFF "b"
 #define FSR_ON "c"
@@ -8,6 +14,19 @@
 #define TMP_OFF "f"
 
 Thread fsrThread = Thread(); // For measuring Pressure data in other thread
+
+int countFSRThread = 0;
+int count_0 = 0;
+int count_1 = 0;
+int count_2 = 0;
+int count_3 = 0;    
+
+int initPosture_0;
+int initPosture_1;
+int initPosture_2;
+int initPosture_3;
+
+float percentage = 0.25; 
 
 //  Mosfet channel
 int MOSFET_C1 = 10;
@@ -64,6 +83,18 @@ void loop() {
   }else if(response.equals(FSR_OFF)){
     Serial.println(response);
     fsrThread.enabled = false;
+    
+    initPosture_0 = 0;
+    initPosture_1 = 1;
+    initPosture_2 = 2;
+    initPosture_3 = 3;
+    
+    countFSRThread = 0;
+    count_0 = 0;
+    count_1 = 0;
+    count_2 = 0;
+    count_3 = 0;
+    
   }else if(response.equals(TMP_ON)){
     Serial.println(response);
     isTMPOn = true;
@@ -82,6 +113,7 @@ void loop() {
 
 // Runnable for fsrThread
 void readPressure(){
+  countFSRThread++;
   int frontRight = analogRead(PRESSURE_0)*5;
   int frontLeft = analogRead(PRESSURE_1)*5;
   int backRight = analogRead(PRESSURE_2)*5;
@@ -90,24 +122,30 @@ void readPressure(){
   Serial.println("1 : "+ String(frontLeft));
   Serial.println("2 : "+ String(backRight));
   Serial.println("3 : "+ String(backLeft));
-
-  // send 'posture msg' to android
-  if(frontRight>2000){
-    BTSerial.println("fr");
-  }
-  if(frontLeft>2000){
-    BTSerial.println("fl");
-  }
-  if(backRight>2000){
-    BTSerial.println("br");
-  }
-  if(backLeft>2000){
-    BTSerial.println("bl");
+  
+  if(countFSRThread <= 5){
+    initPosture_0 = frontRight;
+    initPosture_1 = frontLeft;
+    initPosture_2 = backRight;
+    initPosture_3 = backLeft;
+  }else{
+    // 자세 측정 및 알람
+    Serial.println("your initial post : "+String(initPosture_0));
+    // send 'posture msg' to android
+    if(frontRight>initPosture_0*(percentage+1)){
+      count_0++;
+      if(count_0 >= 5){
+        BTSerial.println("fr");
+        count_0 = 0;
+      }
+    }else{
+      count_0 = 0;
+    }  
   }
 }
 
 void readTmp(){
     digitalWrite(MOSFET_C1,HIGH);
-    Serial.println("temperature on"));  
+    Serial.println("temperature on");  
 }
 
